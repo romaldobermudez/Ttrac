5e227f141be1988741f8bb9842b4eb01cabab09d
romaldobermudez <132368543+romaldobermudez@users.noreply.github.com>
2023-08-01 23:55:30 -0600

    vistas y login

--- Ttrac.Presentacion/Ttrac.Presentacion/Pages/Inicio/Ingreso.cshtml.cs
+++ Ttrac.Presentacion/Ttrac.Presentacion/Pages/Inicio/Ingreso.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Ttrac.Presentacion.Pages._Generales
@@ -6,6 +7,33 @@ namespace Ttrac.Presentacion.Pages._Generales
     {
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost(string email, string password)
+        {
+            // Validar las credenciales de prueba (correo y contraseña)
+            if (email == "helberth.roman@tecoop.cr" && password == "H.Roman2023")
+            {
+                // Crear una cookie de sesión para mantener al usuario autenticado
+                HttpContext.Response.Cookies.Append("LoggedIn", "true");
+
+                return RedirectToPage("/Estadisticas/Estadisticas");
+            }
+            else
+            {
+                // Credenciales incorrectas, mostrar mensaje de error
+                ModelState.AddModelError(string.Empty, "Credenciales incorrectas.");
+                return Page();
+            }
+        }
+
+        public IActionResult OnPostLogout()
+        {
+            // Eliminar la cookie de sesión para cerrar la sesión
+            HttpContext.Response.Cookies.Delete("LoggedIn");
+
+            // Redirigir a la página de inicio de sesión
+            return RedirectToPage("/Inicio/Ingreso");
         }
     }
 }
